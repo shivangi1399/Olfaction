7c664e957f43471ca994f4e16659a63feaaabf64
Shivangi Patel <62049566+shivangi1399@users.noreply.github.com>
2021-04-18 19:16:32 +0530

    Update Plotting.m

--- Plotting.m
+++ Plotting.m
@@ -7,23 +7,39 @@ cd(path);
 
 load('odvar1_1.mat')
 load('odvar2_1.mat')
+load('odvar3_1.mat')
+load('odvar4_1.mat')
+
+y = linspace(0,3000,100);
 
 figure;
-plot(y,odvar1_1)
-hold on
-plot(y,odvar2_1)
-
-
-% for i = 1:length(y)
-%     scatter (odvar1_1(i))
-%     hold on
-%     scatter (odvar2_1(i))
-% end
-
+for k = 1:length(y)
+    scatter(y(k),odvar1_1(k), 'r');hold on
+    scatter(y(k),odvar2_1(k), 'b');hold on
+    drawnow
+    legend({'y=odvar1_1','y = odvar2_1'})
+end
 
 figure;
-for y = linspace(0,3000,100)
-    plot(y, odvar1_1, 'g*'), hold on
-    drawnow;
-    pause(0.1)
+for k = 1:length(y)
+    scatter(y(k),odvar3_1(k), 'b');hold on
+    scatter(y(k),odvar4_1(k), 'r');hold on
+    drawnow
+    legend({'y=odvar3_1','y = odvar4_1'})
+end
+
+figure;
+for k = 1:length(y)
+    scatter(y(k),odvar1_1(k), 'b');hold on
+    scatter(y(k),odvar3_1(k), 'r');hold on
+    drawnow
+    legend({'y=odvar1_1','y = odvar3_1'})
+end
+
+figure;
+for k = 1:length(y)
+    scatter(y(k),odvar2_1(k), 'b');hold on
+    scatter(y(k),odvar4_1(k), 'r');hold on
+    drawnow
+    legend({'y=odvar2_1','y = odvar4_1'})
 end
